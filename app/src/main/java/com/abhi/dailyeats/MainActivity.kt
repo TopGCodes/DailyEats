4cb303f0ad7f6e5b353a2fa68ffac9fb4af596be
TopGCodes <abhishikth159@gmail.com>
2025-08-30 20:52:59 +0530

    Customized the app themes using material design 3

--- app/src/main/java/com/abhi/dailyeats/MainActivity.kt
+++ app/src/main/java/com/abhi/dailyeats/MainActivity.kt
@@ -1,25 +1,26 @@
 package com.abhi.dailyeats
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.abhi.dailyeats.ui.theme.DailyEatsTheme
+import com.example.compose.DailyEatsAppTheme
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DailyEatsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            DailyEatsAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize().fillMaxWidth()) { innerPadding ->
+
+
 
                 }
             }
